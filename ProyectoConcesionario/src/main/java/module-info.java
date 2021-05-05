f73785f9aa841fdf549f7af5cc3329f2de7f577d
gbret <gbret@192.168.1.9>
2021-05-05 09:17:17 +0200

    Creacion dao e interfaces

--- ProyectoConcesionario/src/main/java/module-info.java
+++ ProyectoConcesionario/src/main/java/module-info.java
@@ -1,7 +1,11 @@
 module es.iesfranciscodelosrios.ProyectoConcesionario {
     requires javafx.controls;
     requires javafx.fxml;
-
+	requires java.sql;
+	requires java.xml.bind;
+    requires com.sun.xml.bind;
     opens es.iesfranciscodelosrios.ProyectoConcesionario to javafx.fxml;
+    opens conexion to com.sun.xml.bind, java.xml.bind;
+    opens utils to com.sun.xml.bind, java.xml.bind;
     exports es.iesfranciscodelosrios.ProyectoConcesionario;
 }
